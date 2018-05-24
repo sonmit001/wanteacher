ec1eb2df770f4d12207a481f057e75163f5b54d8
김완기 <kimwg@sisnet.co.kr>
2018-05-24 14:36:44 +0900

    Vue.js - Chapter #1 ~ #5

--- dataTable/src/main/java/kr/co/solution/controller/VueController.java
+++ dataTable/src/main/java/kr/co/solution/controller/VueController.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VueController {
-	
-	@RequestMapping("vueTest")
-	public String vueTest() {
-		return "vue/vueTest";
-	}
+public class VueController{
+		
 }
